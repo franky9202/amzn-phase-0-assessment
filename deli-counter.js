8b054f9ae089e63b97fb06e1260e9d2ab55a59da
Edgar <franky9202@gmail.com>
2022-04-10 21:00:22 -0700

    Assignment

--- deli-counter.js
+++ deli-counter.js
@@ -1,6 +1,19 @@
 // 1. Write your functions here
-
-
+const katzDeli = []
+const numbofClients = [1, 2, 3]
+function line(katzDeli){
+    if (i > string.length, i++){
+        return `The line is currently: ${numbofClients[i]} ${string[i]}`
+    }
+    return "The line is currently empty."
+}
+function takeANumber(katzDeli, string){
+console.log()
+}
+function nowServing(){
+    const removeClient = katzDeli.slice()
+    console.log("There is nobody waiting to be served!")
+}
 // 2. Example Usage
 
 // const katzDeli = []
@@ -9,7 +22,7 @@
 // takeANumber(katzDeli, "Grace") //=> Welcome, Grace. You are number 2 in line.
 // takeANumber(katzDeli, "Kent") //=> Welcome, Kent. You are number 3 in line.
 
-// line(katzDeli) //=> "The line is currently: 1. Ada 2. Grace 3. Kent"
+console.log(line(katzDeli)) //=> "The line is currently: 1. Ada 2. Grace 3. Kent"
 
 // nowServing(katzDeli) //=> "Currently serving Ada."
 
@@ -22,3 +35,7 @@
 // nowServing(katzDeli) //=> "Currently serving Grace."
 
 // line(katzDeli) //=> "The line is currently: 1. Kent 2. Matz"
+
+// input array
+// output returns a string
+// sideeffects
